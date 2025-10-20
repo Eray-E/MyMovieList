d71dd200bcc84d8f36c45ddb5726ee89a65bd017
ErayElman <erayelman@gmail.com>
2025-10-20 12:04:21 +0200

    UPDATED

--- src/main/java/com/example/demo/Movie.java
+++ src/main/java/com/example/demo/Movie.java
@@ -13,6 +13,30 @@ public class Movie {
         this.releaseYear = releaseYear;
         this.watched = watched;
     }
+
+    public boolean isWatched() {
+        return watched;
+    }
+    public void setWatched(boolean watched) {
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
 }
 
 
