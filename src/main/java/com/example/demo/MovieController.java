a4dbf5d14062bcd4a67cd7bdeae07c83cfcb90ce
ErayElman <erayelman@gmail.com>
2025-11-24 00:29:49 +0100

    Cross Origin Befehl

--- src/main/java/com/example/demo/MovieController.java
+++ src/main/java/com/example/demo/MovieController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import java.util.List;
 @RestController
 public class MovieController {
 
+    @CrossOrigin
     @GetMapping("/todos")
     public List<Movie> getMovies(){
         return List.of(new Movie("M1","M2",2025,true));
